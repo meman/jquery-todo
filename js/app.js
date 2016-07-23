e3d0a831e11ca1e076cd8bdf58eb2a1760a4eac5
Dan <camina@dans-MBP-3.home>
2016-07-23 22:33:36 +0100

    Created method to add new todo to a static array

--- js/app.js
+++ js/app.js
@@ -2,11 +2,45 @@
   'use strict';
 
   var app = {
+    init:function(){
+      this.$cachedTodoApp = $('#todo-app');
 
+      // will need to create a proper store !!!
+      this.todos = [];
+
+      this.bindEvents();
+    },
+    bindEvents:function(){
+      this.$cachedTodoApp.find('#new-todo')
+        .on('keyup',this.addTodo.bind(this));
+    },
+    addTodo:function(evt){
+      var $input = evt.target,
+          val = $input.value.trim();
+
+      // key 13 is enter
+
+      if(evt.keyCode !== 13 || !val){
+        return;
+      }
+
+      // need to add uuid
+
+      var newTodo = {
+        id:this.todos.length + 1,
+        text:val,
+        completed:false
+      };
+
+      this.todos.push(newTodo);
+
+      $input.value = '';
+
+    }
   };
 
-  // $(function(){
-  //   console.log('ready');
-  // });
+  $(function(){
+    app.init();
+  });
 
 })(jQuery);
