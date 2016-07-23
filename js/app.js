6c2ef50159d2a2d974594a5ea3658aa606a2c68b
Dan <camina@dans-MBP-3.home>
2016-07-24 00:10:17 +0100

    Update todo functionality

--- js/app.js
+++ js/app.js
@@ -17,7 +17,10 @@
 
       this.$cachedTodoApp.find('#todo-list')
         .on('change','.toggle',this.toggleTodoComplete.bind(this))
-        .on('click','.destroy',this.removeTodo.bind(this));
+        .on('click','.destroy',this.removeTodo.bind(this))
+        .on('dblclick','label',this.toggleEditing.bind(this))
+        .on('keyup','.edit',this.updateKeyup)
+        .on('focusout','.edit',this.updateTodo.bind(this));
     },
     addTodo:function(evt){
       var $input = evt.target,
@@ -60,6 +63,47 @@
       this.todos = this.todos.filter(function(todo){
         return todo.id != id;
       });
+      this.render();
+    },
+    toggleEditing:function(evt){
+      var $todoElm = $(evt.target).parents('li'),
+          selectedTodoText = this.todos.find(function(todo){
+            return todo.id == $todoElm.attr('id');}).text;
+      $todoElm.addClass('editing');
+      $todoElm.find('.edit').val(selectedTodoText).focus();
+    },
+    updateKeyup:function(evt){
+      var $target = $(evt.target);
+      var keyCode = evt.keyCode;
+
+      if(keyCode === 13){
+        $target.blur();
+      }
+      if(keyCode === 27){
+        $target.data('abort',true);
+        $target.blur();
+      }
+    },
+    updateTodo:function(evt){
+      var $target = $(evt.target),
+          val = $target.val().trim();
+
+
+      if(!val){
+        this.removeTodo(evt);
+        return;
+      }
+
+      if(!$target.data().abort){
+        this.todos = this.todos.map(function(todo){
+          if(todo.id == $target.closest('li').attr('id')){
+            todo.text = val;
+          }
+          return todo;
+        });
+      }
+      $target.data().abort = false;
+
       this.render();
     },
     render:function(){
